aa86b985091673f78751f63460502b712c0bd099
markkizz <mark.pattanapara@gmail.com>
2020-10-24 20:24:49 +0700

    update: change gunicorn config

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8 -*-
-bind = '0.0.0.0:5005'
-workers = 1
+import os
+from string import Template
+# bind = '0.0.0.0:5005'
+bind = Template('0.0.0.0:$port').substitute(port=os.getenv('PORT') if os.getenv('PORT') else '5005')
+workers = 3
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
